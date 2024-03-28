5ff83f05d2f4588cdea47be5dd74b5b06fc036ac
Heri Susanto <herisusanto.app@gmail.com>
2024-03-29 05:45:21 +0700

    Set up Prisma, Docker compose and auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,34 @@ import { AppService } from './app.service';
 import { ChatModule } from './chat/chat.module';
 import { RoomsController } from './chat/rooms.controller';
 import { ChatWebsocketGateway } from './chat/chat.websocket.gateway';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver } from '@nestjs/apollo';
+import { join } from 'path';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [ChatModule],
+  imports: [
+    ChatModule,
+    AuthModule,
+    UserModule,
+    GraphQLModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      driver: ApolloDriver,
+      useFactory: async (configService: ConfigService) => {
+        return {
+          playground: true,
+          autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+          sortSchema: true,
+        };
+      },
+    }),
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [RoomsController],
   providers: [ChatWebsocketGateway],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,40 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ValidationPipe, BadRequestException } from '@nestjs/common';
+import * as cookieParser from 'cookie-parser';
+import * as graphqlUploadExpress from 'graphql-upload/graphqlUploadExpress.mjs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    allowedHeaders: [
+      'Accept',
+      'Authorization',
+      'Content-Type',
+      'X-Requested-With',
+      'apollo-require-preflight',
+    ],
+    methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  });
+  app.use(cookieParser());
+  // app.use(graphqlUploadExpress({ maxFileSize: 10000000000, maxFiles: 1 }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      exceptionFactory: (errors) => {
+        const formattedErrors = errors.reduce((accumulator, error) => {
+          accumulator[error.property] = Object.values(error.constraints).join(
+            ', ',
+          );
+          return accumulator;
+        }, {});
+        throw new BadRequestException(formattedErrors);
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
